fa4afbd828ad7458e259e8b7cee9f21dcc1ac8d3
dmfarcas <dan@farcas.me>
2016-05-02 21:31:57 +0300

    Changing typeface on dropdown select almost working

--- nodestuff/views/Default.jsx
+++ nodestuff/views/Default.jsx
@@ -1,25 +1,31 @@
 import React, {Component} from 'react'
-import {render} from 'react-dom'
+import {render, findDOMNode} from 'react-dom'
 import {browserHistory, Router, Route, Link} from 'react-router'
 
 class App extends Component {
     render() {
         return (
             <div>
-                <Input/>
-                <ChooseType/>
+                <Input />
             </div>
 
         )
     }
 }
 
-export class ChooseType extends Component {
+class ChooseType extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            types: ""
+            types: "",
+            chosenType: "Caligraphy"
         };
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(event) {
+        this.setState({chosenType: event.target.value});
+        $(document).trigger("UpdateType");
     }
 
     componentDidMount() {
@@ -31,10 +37,17 @@ export class ChooseType extends Component {
     render() {
         let results = this.state.types;
                 return(
-                    <select>
+                    <select
+                        onChange={this.handleChange}
+                        value={this.state.value}
+                        ref="testpls">
                     { Object.keys(results).map(function (key) {
                         return (
-                            <option key={key}>{results[key]}</option>
+                            <option
+                                key={key}
+                                value={results[key].slice(0,-4)}>
+                                    {results[key].slice(0,-4)}
+                            </option>
                         );
                     }, this)}
                 </select>
@@ -47,20 +60,31 @@ class Input extends Component {
     constructor(props) {
         super(props);
         this.getAscii = this.getAscii.bind(this)
+        this.getAscii();
     }
     getAscii(event) {
+        let sendThisThing = event ? event.target.value : "Hello, world!"
         $.post("makeascii", {
-            ascii: event.target.value,
-            font: 'Caligraphy'
+            ascii: sendThisThing,
+            font: this.refs.chooseType ? this.refs.chooseType.state.chosenType : 'Basic'
         }, (data) => {
             this.refs.asciiArt.update(data);
         });
     }
 
+    componentDidMount() {
+        $(document).on("UpdateType", (event, type) => {
+            console.log(event);
+            this.getAscii(type);
+        });
+    }
     render() {
         return (
             <div>
-                <input type="text" onChange={this.getAscii}/>
+                <input type="text"
+                    onChange={this.getAscii} 
+                    defaultValue="Hello, world!"/>
+                <ChooseType ref='chooseType' />
                 <DisplayAscii ref='asciiArt' />
             </div>
         )
